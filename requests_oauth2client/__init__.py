620fd05360845d3f0a44f7eab6bc9ba15eb788e3
Guillaume Pujol <guillaume.pujol@axa.com>
2020-07-03 14:26:32 +0200

    export BearerToken at module top level

--- requests_oauth2client/__init__.py
+++ requests_oauth2client/__init__.py
@@ -6,3 +6,4 @@ from .authorization_code import AuthorizationCodeHandler
 from .client import OAuth2Client
 from .client_authentication import (ClientSecretBasic, ClientSecretJWT,
                                     ClientSecretPost, PrivateKeyJWT)
+from .token_response import BearerToken, BearerTokenEndpointResponse
