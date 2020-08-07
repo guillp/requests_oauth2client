9cbc72a37424416face1593bd8417dee7af023c3
Guillaume Pujol <guillaume.pujol@axa.com>
2020-08-07 21:54:59 +0200

    use requests_mock based test

--- tests/test_authorization_code.py
+++ tests/test_authorization_code.py
@@ -1,24 +1,33 @@
-import os
-from pathlib import Path
+import base64
+import hashlib
+import secrets
+from datetime import datetime, timedelta
+from urllib.parse import parse_qs
 
-from dotenv import load_dotenv
+import requests
+from furl import furl
 
-from requests_oauth2client import ClientSecretPost, OAuth2Client
+from requests_oauth2client import BearerTokenEndpointResponse, ClientSecretPost, OAuth2Client
 from requests_oauth2client.authorization_code import AuthorizationCodeHandler, PkceHelper
 from requests_oauth2client.discovery import oidc_discovery_document_url
 
-load_dotenv(Path(__file__).with_name("authorization_code.env"))
+client_id = "TEST_CLIENT_ID"
+client_secret = "TEST_CLIENT_SECRET"
+issuer = "https://test.com"
+redirect_uri = "TEST_REDIRECT_URI"
+audience = "TEST_AUDIENCE"
+scope = "TEST_SCOPE"
 
-client_id = os.getenv("TEST_CLIENT_ID")
-client_secret = os.getenv("TEST_CLIENT_SECRET")
-issuer = os.getenv("TEST_ISSUER")
-redirect_uri = os.getenv("TEST_REDIRECT_URI")
-audience = os.getenv("TEST_AUDIENCE")
-scope = os.getenv("TEST_SCOPE")
+discovery_document = {
+    "authorization_endpoint": furl(issuer, path="/authorize").url,
+    "token_endpoint": furl(issuer, path="/token").url,
+}
 
 
-def test_authorization_code(session):
+def test_authorization_code(session, requests_mock):
+
     discovery_url = oidc_discovery_document_url(issuer)
+    requests_mock.get(discovery_url, json=discovery_document)
     discovery = session.get(discovery_url).json()
     authorization_endpoint = discovery.get("authorization_endpoint")
     assert authorization_endpoint
@@ -32,17 +41,52 @@ def test_authorization_code(session):
         scope=scope,
         audience=audience,
     )
-    print(authorization_request.request.url)
-    params = input("Callback url or params: ")
-    code = authorization_request.validate_callback(params)
+
+    authorization_code = secrets.token_urlsafe()
+
+    def authorization_response_callback(request, context):
+        assert request.qs.get("client_id")[0] == client_id
+        assert request.qs.get("response_type")[0] == "code"
+        assert request.qs.get("redirect_uri")[0] == redirect_uri
+        state = request.qs.get("state")
+        context.status_code = 302
+        query = {"code": authorization_code}
+        if state:
+            query["state"] = state
+        context.headers = {"Location": furl(redirect_uri, query=query).url}
+        return "redirection"
+
+    requests_mock.get(authorization_request.request.url, text=authorization_response_callback)
+    resp = requests.get(authorization_request.request.url, allow_redirects=False)
+    location = resp.headers.get("Location")
+    code = authorization_request.validate_callback(location)
 
     client = OAuth2Client(token_endpoint, ClientSecretPost(client_id, client_secret))
+
+    access_token = secrets.token_urlsafe()
+
+    def token_response_callback(request, context):
+        params = parse_qs(request.text)
+        assert params.get("client_id")[0] == client_id
+        assert params.get("client_secret")[0] == client_secret
+        assert params.get("grant_type")[0] == "authorization_code"
+        assert params.get("code")[0] == authorization_code
+
+        return {"access_token": access_token, "token_type": "Bearer", "expires_in": 3600}
+
+    requests_mock.post(
+        token_endpoint, json=token_response_callback,
+    )
     token = client.authorization_code(code=code, redirect_uri=redirect_uri)
-    print(token)
+    assert isinstance(token, BearerTokenEndpointResponse)
+    assert token.access_token == access_token
+    assert not token.is_expired()
+    assert token.expires_at == datetime.now() + timedelta(seconds=3600)
 
 
-def test_authorization_code_pkce(session):
+def test_authorization_code_pkce(session, requests_mock):
     discovery_url = oidc_discovery_document_url(issuer)
+    requests_mock.get(discovery_url, json=discovery_document)
     discovery = session.get(discovery_url).json()
     authorization_endpoint = discovery.get("authorization_endpoint")
     assert authorization_endpoint
@@ -58,12 +102,61 @@ def test_authorization_code_pkce(session):
         audience=audience,
         code_verifier=code_verifier,
     )
-    print(authorization_request.request.url)
-    params = input("Callback url or params: ")
-    code = authorization_request.validate_callback(params)
+
+    authorization_code = secrets.token_urlsafe()
+
+    def authorization_response_callback(request, context):
+        assert request.qs.get("client_id")[0] == client_id
+        assert request.qs.get("response_type")[0] == "code"
+        assert request.qs.get("redirect_uri")[0] == redirect_uri
+
+        code_challenge = request.qs.get("code_challenge")
+        code_challenge_method = request.qs.get("code_challenge_method")
+
+        assert len(code_challenge) == 1
+        code_challenge = code_challenge[0]
+        assert code_challenge_method == ["S256"]
+        assert (
+            base64.urlsafe_b64encode(hashlib.sha256(code_verifier.encode()).digest()).rstrip(
+                b"="
+            )
+            == code_challenge.encode()
+        )
+
+        state = request.qs.get("state")
+        context.status_code = 302
+        query = {"code": authorization_code}
+        if state:
+            query["state"] = state
+        context.headers = {"Location": furl(redirect_uri, query=query).url}
+        return "redirection"
+
+    requests_mock.get(authorization_request.request.url, text=authorization_response_callback)
+    resp = requests.get(authorization_request.request.url, allow_redirects=False)
+    location = resp.headers.get("Location")
+    code = authorization_request.validate_callback(location)
 
     client = OAuth2Client(token_endpoint, ClientSecretPost(client_id, client_secret))
+
+    access_token = secrets.token_urlsafe()
+
+    def token_response(request, context):
+        params = parse_qs(request.text)
+        assert params.get("client_id")[0] == client_id
+        assert params.get("client_secret")[0] == client_secret
+        assert params.get("grant_type")[0] == "authorization_code"
+        assert params.get("code")[0] == authorization_code
+        assert params.get("code_verifier")[0] == code_verifier
+
+        return {"access_token": access_token, "token_type": "Bearer", "expires_in": 3600}
+
+    requests_mock.post(
+        token_endpoint, json=token_response,
+    )
     token = client.authorization_code(
         code=code, redirect_uri=redirect_uri, code_verifier=code_verifier
     )
-    print(token)
+    assert isinstance(token, BearerTokenEndpointResponse)
+    assert token.access_token == access_token
+    assert not token.is_expired()
+    assert token.expires_at == datetime.now() + timedelta(seconds=3600)
--- tests/test_client_credentials.py
+++ tests/test_client_credentials.py
@@ -1,29 +1,52 @@
-import os
-from pathlib import Path
+import secrets
+from urllib.parse import parse_qs
 
 import requests
-from dotenv import load_dotenv
 
 from requests_oauth2client import ClientSecretPost, OAuth2Client, OAuth2ClientCredentialsAuth
 
-load_dotenv(Path(__file__).with_name("client_credentials.env"))
-
-client_id = os.getenv("TEST_CLIENT_ID")
-client_secret = os.getenv("TEST_CLIENT_SECRET")
-token_endpoint = os.getenv("TEST_TOKEN_ENDPOINT")
-audience = os.getenv("TEST_AUDIENCE")
-api = os.getenv("TEST_API")
+client_id = "TEST_CLIENT_ID"
+client_secret = "TEST_CLIENT_SECRET"
+token_endpoint = "https://test.com/token"
+api = "https://test.com/api"
 
 
-def test_client_credentials_get_token():
+def test_client_credentials_get_token(requests_mock):
+    access_token = secrets.token_urlsafe()
+
+    def token_response_callback(request, context):
+        params = parse_qs(request.text)
+        assert params.get("client_id")[0] == client_id
+        assert params.get("client_secret")[0] == client_secret
+        assert params.get("grant_type")[0] == "client_credentials"
+
+        return {"access_token": access_token, "token_type": "Bearer", "expires_in": 3600}
+
+    requests_mock.post(
+        token_endpoint, json=token_response_callback,
+    )
     client = OAuth2Client(token_endpoint, ClientSecretPost(client_id, client_secret))
-    token_response = client.client_credentials(audience=audience)
-    assert token_response.access_token
-    print(token_response.access_token)
+    token_response = client.client_credentials()
+    assert token_response.access_token == access_token
 
 
-def test_client_credentials_api():
+def test_client_credentials_api(requests_mock):
+    access_token = secrets.token_urlsafe()
+
+    def token_response_callback(request, context):
+        params = parse_qs(request.text)
+        assert params.get("client_id")[0] == client_id
+        assert params.get("client_secret")[0] == client_secret
+        assert params.get("grant_type")[0] == "client_credentials"
+
+        return {"access_token": access_token, "token_type": "Bearer", "expires_in": 3600}
+
+    requests_mock.post(
+        token_endpoint, json=token_response_callback,
+    )
     client = OAuth2Client(token_endpoint, ClientSecretPost(client_id, client_secret))
-    auth = OAuth2ClientCredentialsAuth(client, audience=audience)
+    auth = OAuth2ClientCredentialsAuth(client)
+
+    requests_mock.get(api, request_headers={"Authorization": f"Bearer {access_token}"})
     response = requests.get(api, auth=auth)
     assert response.ok
