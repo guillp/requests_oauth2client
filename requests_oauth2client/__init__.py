dff9da3256ad9ebb35e258e94365b5292378d613
Guillaume Pujol <guillaume.pujol@axa.com>
2021-08-19 11:06:58 +0200

    expose exceptions and utils at root module level

--- requests_oauth2client/__init__.py
+++ requests_oauth2client/__init__.py
@@ -8,4 +8,14 @@ from .client_authentication import (ClientSecretBasic, ClientSecretJWT,
 from .device_authorization import DeviceAuthorizationClient, DeviceAuthorizationPoolingJob
 from .discovery import (oauth2_discovery_document_url,
                         oidc_discovery_document_url, well_known_uri)
+from .exceptions import (AccessDenied, AccountSelectionRequired, AuthorizationPending,
+                         AuthorizationResponseError, ConsentRequired, DeviceAuthorizationError,
+                         ExpiredDeviceCode, ExpiredToken, InteractionRequired,
+                         InvalidDeviceAuthorizationResponse, InvalidGrant, InvalidIdToken,
+                         InvalidJWT, InvalidScope, InvalidTokenResponse, InvalidUrl,
+                         LoginRequired, OAuth2Error, ServerError, SessionSelectionRequired,
+                         SlowDown, TokenResponseError, UnauthorizedClient,
+                         UnknownTokenResponseError, UnsupportedTokenType)
 from .tokens import BearerToken, IdToken
+from .utils import (b64_encode, b64u_decode, b64u_encode,
+                    generate_jwk_key_pair, sign_jwt, validate_url)
