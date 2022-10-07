1f3db07522bbae9d0c84808037ffe5e9af5b4e3d
Guillaume Pujol <guillaume.pujol@axa.com>
2022-10-07 17:57:12 +0200

    introduce PingClient

---
+++ requests_oauth2client/vendor_specific/ping.py
@@ -0,0 +1,51 @@
+"""PingID specific client."""
+from typing import Optional, Tuple, Union
+
+import requests
+
+from requests_oauth2client import OAuth2Client
+
+
+class PingClient(OAuth2Client):
+    """A client for PingID Authorization Server.
+
+    This will initialize all endpoints with the PingID specific urls, without using the metadata.
+    Excepted for avoiding a round-trip to get the metadata url, this does not provide any advantage over using
+    `OAuth2Client.from_discovery_endpoint(issuer="https://myissuer.domain.tld")`
+    """
+
+    def __init__(
+        self,
+        issuer: str,
+        auth: Union[requests.auth.AuthBase, Tuple[str, str], str, None] = None,
+        client_id: Optional[str] = None,
+        client_secret: Optional[str] = None,
+        session: Optional[requests.Session] = None,
+    ):
+        if not issuer.startswith("https://"):
+            if issuer.__contains__("://"):
+                raise ValueError("Invalid issuer, must be an https:// url or a domain name")
+            issuer = f"https://{issuer}"
+        if "." not in issuer:
+            raise ValueError(
+                "Invalid issuer. It must contain at least a dot in the domain name"
+            )
+
+        super().__init__(
+            authorization_endpoint=f"{issuer}/as/authorization.oauth2",
+            token_endpoint=f"{issuer}/as/token.oauth2",
+            revocation_endpoint=f"{issuer}/as/revoke_token.oauth2",
+            userinfo_endpoint=f"{issuer}/idp/userinfo.openid",
+            introspection_endpoint=f"{issuer}/as/introspect.oauth2",
+            jwks_uri=f"{issuer}/pf/JWKS",
+            registration_endpoint=f"{issuer}/as/clients.oauth2",
+            ping_revoked_sris_endpoint=f"{issuer}/pf-ws/rest/sessionMgmt/revokedSris",
+            ping_session_management_sris_endpoint=f"{issuer}/pf-ws/rest/sessionMgmt/sessions",
+            ping_session_management_users_endpoint=f"{issuer}/pf-ws/rest/sessionMgmt/users",
+            ping_end_session_endpoint=f"{issuer}/idp/startSLO.ping",
+            device_authorization_endpoint=f"{issuer}/as/device_authz.oauth2",
+            auth=auth,
+            client_id=client_id,
+            client_secret=client_secret,
+            session=session,
+        )
---
+++ tests/unit_tests/vendor_specific/test_ping.py
@@ -0,0 +1,36 @@
+from requests_oauth2client.vendor_specific.ping import PingClient
+
+
+def test_ping_client() -> None:
+    ping_client = PingClient("mydomain.tld", auth=("client_id", "client_secret"))
+    assert ping_client.token_endpoint == "https://mydomain.tld/as/token.oauth2"
+    assert ping_client.authorization_endpoint == "https://mydomain.tld/as/authorization.oauth2"
+    assert ping_client.token_endpoint == "https://mydomain.tld/as/token.oauth2"
+    assert ping_client.revocation_endpoint == "https://mydomain.tld/as/revoke_token.oauth2"
+    assert ping_client.userinfo_endpoint == "https://mydomain.tld/idp/userinfo.openid"
+    assert ping_client.introspection_endpoint == "https://mydomain.tld/as/introspect.oauth2"
+    assert ping_client.jwks_uri == "https://mydomain.tld/pf/JWKS"
+    assert (
+        ping_client.extra_metadata["registration_endpoint"]
+        == "https://mydomain.tld/as/clients.oauth2"
+    )
+    assert (
+        ping_client.extra_metadata["ping_revoked_sris_endpoint"]
+        == "https://mydomain.tld/pf-ws/rest/sessionMgmt/revokedSris"
+    )
+    assert (
+        ping_client.extra_metadata["ping_session_management_sris_endpoint"]
+        == "https://mydomain.tld/pf-ws/rest/sessionMgmt/sessions"
+    )
+    assert (
+        ping_client.extra_metadata["ping_session_management_users_endpoint"]
+        == "https://mydomain.tld/pf-ws/rest/sessionMgmt/users"
+    )
+    assert (
+        ping_client.extra_metadata["ping_end_session_endpoint"]
+        == "https://mydomain.tld/idp/startSLO.ping"
+    )
+    assert (
+        ping_client.device_authorization_endpoint
+        == "https://mydomain.tld/as/device_authz.oauth2"
+    )
