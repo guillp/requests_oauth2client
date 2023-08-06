59302ec4b8e875937daf4e221e3f27ab76171909
Guillaume <guillaume@local.host>
2023-08-06 16:38:57 +0200

    fix flask tests

--- tests/unit_tests/test_flask.py
+++ tests/unit_tests/test_flask.py
@@ -54,19 +54,15 @@ def test_flask(
         assert resp.json == json_resp
         resp = client.get("/api")
         assert resp.json == json_resp
-        # api_client.session.auth.token = None  # strangely this has no effect in a test session
-        with client.session_transaction() as sess:  # does what 'api_client.session.auth.token = None' should do
-            sess.pop("session_key", None)
+        api_client.session.auth.token = None
         # this should trigger a new token request then the API request
         resp = client.get("/api")
         assert resp.json == json_resp
 
-    assert len(requests_mock.request_history) == 5
-    token_request = requests_mock.request_history[0]
-    api_request1 = requests_mock.request_history[1]
-    api_request2 = requests_mock.request_history[2]
+    assert api_client.session.auth.token == access_token
 
-    token_params = parse_qs(token_request.text)
+    token_request1 = requests_mock.request_history[0]
+    token_params = parse_qs(token_request1.text)
     assert token_params.get("client_id") == [client_id]
     if not scope:
         assert token_params.get("scope") is None
@@ -76,5 +72,23 @@ def test_flask(
         assert token_params.get("scope") == [" ".join(scope)]
     assert token_params.get("client_secret") == [client_secret]
 
+    api_request1 = requests_mock.request_history[1]
     assert api_request1.headers.get("Authorization") == f"Bearer {access_token}"
+
+    api_request2 = requests_mock.request_history[2]
     assert api_request2.headers.get("Authorization") == f"Bearer {access_token}"
+
+    token_request2 = requests_mock.request_history[3]
+    token_params = parse_qs(token_request2.text)
+    assert token_params.get("client_id") == [client_id]
+    if not scope:
+        assert token_params.get("scope") is None
+    elif isinstance(scope, str):
+        assert token_params.get("scope") == [scope]
+    elif isinstance(scope, Iterable):
+        assert token_params.get("scope") == [" ".join(scope)]
+    assert token_params.get("client_secret") == [client_secret]
+
+    api_request3 = requests_mock.request_history[4]
+    assert api_request3.headers.get("Authorization") == f"Bearer {access_token}"
+
